636c30d17d53d47c266c47ad46385125ac0a7a69
Rasmus Jakobsen <buhl.jakobsen@gmail.com>
2022-06-16 11:48:22 +0200

    wishlist fix

--- src/context/WishlistContext.js
+++ src/context/WishlistContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer} from "react";
+import { createContext, useReducer } from "react";
 
 // context is created
 export const WishlistContext = createContext();
@@ -7,10 +7,10 @@ export const WishlistContext = createContext();
 const wishlistReducer = (state, action) => {
   switch (action.type) {
     case "ADD_ITEM":
-        // Adds the pssed item to the current array of items
+      // Adds the pssed item to the current array of items
       return { ...state, wishlist: [...state.wishlist, action.payload] };
     case "REMOVE_ITEM":
-        // filters the array, only returning items with id not matching the passed id
+      // filters the array, only returning items with id not matching the passed id
       return {
         ...state,
         wishlist: [
@@ -26,27 +26,29 @@ const wishlistReducer = (state, action) => {
 
 // provider is created to wrap the parts of the app that should have access to its values
 export function WishlistProvider({ children }) {
-    // initial wishlist is retieved from local storage
+  // initial wishlist is retieved from local storage
   const wishlist = JSON.parse(localStorage.getItem("wishlisted-movies"));
 
   // initial state and dispatch funtion is created
   const [state, dispatch] = useReducer(wishlistReducer, {
-    wishlist: [...wishlist],
+    wishlist: [...(wishlist ? wishlist : [])],
   });
 
   // dispatch function for adding item to wishlist - decalering type name and payload
-  const addToWishlist = ({id, title, cover}) => {
-    dispatch({ type: "ADD_ITEM", payload: {id, title, cover} });
+  const addToWishlist = ({ id, title, cover }) => {
+    dispatch({ type: "ADD_ITEM", payload: { id, title, cover } });
   };
 
-// dispatch function for removing item to wishlist - decalering type name and payload
+  // dispatch function for removing item to wishlist - decalering type name and payload
   const removeFromWishlist = (id) => {
     dispatch({ type: "REMOVE_ITEM", payload: id });
   };
 
-  // provider return the context and renders its children giving access to its values 
+  // provider return the context and renders its children giving access to its values
   return (
-    <WishlistContext.Provider value={{ ...state, addToWishlist, removeFromWishlist }}>
+    <WishlistContext.Provider
+      value={{ ...state, addToWishlist, removeFromWishlist }}
+    >
       {children}
     </WishlistContext.Provider>
   );
